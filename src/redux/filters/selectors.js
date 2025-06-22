e25a685b3b4c258b7f1d43de009639763614ce27
Mykhailo Pylypiv <developer.mykhailo@gmail.com>
2025-06-22 14:28:06 -0600

    add filter by number

--- src/redux/filters/selectors.js
+++ src/redux/filters/selectors.js
@@ -6,8 +6,12 @@ import { selectContacts } from "../contacts/selectors";
 export const selectFilteredContacts = createSelector(
   [selectContacts, selectNameFilter],
   (contacts, filter) => {
-    return contacts.filter((contact) =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(
+      (contact) =>
+        contact.name.toLowerCase().includes(normalizedFilter) ||
+        contact.number.includes(normalizedFilter)
     );
   }
 );
